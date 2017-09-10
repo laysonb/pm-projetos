b54f2a5ccfc4aa2f2e6c059c49e6b1a69628c69a
Ilandio Joaquim <laysonbelinho@gmail.com>
2017-09-10 20:31:09 +0100

    Projeto 6 concluido

---
+++ pm-proj005/index.php
@@ -0,0 +1,188 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+    <link rel="stylesheet" href="estilo.css" />
+</head>
+<body>
+    
+     <table border="0">
+        <caption>Relatório de Faturamento</caption>
+    
+         <tr>
+             <th>
+             <input type="checkbox" name="checkbox" /> 
+             </th>
+             <th>Coluna1</th>
+             <th>Coluna</th>
+             <th>Coluna3</th>
+             <th>Coluna4</th>
+             <th>Coluna5</th>
+             <th>Coluna6</th>
+             <th>Coluna7</th>     
+         </tr>
+          <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+            </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+                <tr>
+             <td>
+             <input type="checkbox" name="checkbox" />
+             </td>
+             <td>Texto A</td>
+             <td>Texto B</td>
+             <td>Texto C</td>
+             <td>Texto E</td>
+             <td>Texto F</td>
+             <td>Texto G</td>
+             <td>Texto H</td>
+         </tr>
+         <tfoot>
+             <tr>
+                 <td colspan="8">
+                    Registro por página
+                     <select>
+                         <option value="10">10</option>
+                         <option value="20">20</option>
+                         <option value="30">30</option>
+                     </select>
+                     <span>1-10 de 100</span>
+                     <a href="#"><img src="anterior.png"></a>
+                     <a href="#"><img src="proximo.png"></a>
+                 </td>
+             </tr>
+         </tfoot>
+         
+         
+     </table> 
+</body>
+</html>
